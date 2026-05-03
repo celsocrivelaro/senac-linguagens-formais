212fb8c349322ab5049f687e310c2cebaf811ba2
Celso Crivelaro <celsocrivelaro@gmail.com>
2026-05-03 14:34:03 -0300

    Create main.rb

---
+++ parser-early/main.rb
@@ -0,0 +1,16 @@
+require_relative "earley"
+require_relative "gramatica"
+
+regras = [
+  Regra.new('S', %w[N + S]),
+  Regra.new('S', %w[N]),
+  Regra.new('N', %w[1]),
+  Regra.new('N', %w[2]),
+  Regra.new('N', %w[3])
+]
+
+gramatica = Gramatica.new(regras, "S")
+
+parser = EarleyParser.new(gramatica)
+input = "1+2"
+puts parser.parse(input) ? 'Aceito' : 'Rejeitado'
